fabd33fb08e22bff4ef224f80408f83df9723738
tarkeshwar raj <tarkeshwarraj@gmail.com>
2025-06-27 15:46:37 +0530

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
 
 export const loginPage = (req, res) => {
     res.render('admin/login', { layout: false });
@@ -10,8 +11,16 @@ export const adminLogin = (req, res) => {
 export const logout = (req, res) => {
 }
 
-export const allUsers = (req, res) => {
-    res.render('admin/users');
+export const allUsers = async (req, res) => {
+    const users = await User.find();
+const formattedUsers = users.map(user => ({
+  id: user._id, // MAPPED from _id
+  fullname: user.fullname,
+  username: user.username,
+  role: user.role
+}));
+
+res.render("admin/users", { users: formattedUsers });
 
 }
 
@@ -24,20 +33,51 @@ export const settings = (req, res) => {
 }
 
 export const addUserPage = (req, res) => {
-}
-
-export const addUser = (req, res) => {
     res.render('admin/users/create');
+}
+
+export const addUser = async (req, res) => {
+    await User.create(req.body);
+    res.redirect('/admin/users');
 
 }
 
-export const updateUserPage = (req, res) => {
-    res.render('admin/users/update');
-
+export const updateUserPage = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const user = await User.findById(id);
+        if(!user){
+            res.status(404).send('User not found');
+        }
+        res.render('admin/users/update', { user });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
 }
 
-export const updateUser = (req, res) => {
+export const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const { fullname, password, role } = req.body;
+    try {
+        const user = await User.findByIdAndUpdate(id);
+        if(!user){
+            res.status(404).send('User not found');
+        }
+
+        user.fullname = fullname || user.fullname;
+        if(password){
+            user.password = await bcrypt.hash(password, 10);
+        }
+        user.role = role || user.role;
+        await user.save();
+        res.redirect('/admin/users');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
 }
+
 
 export const deleteUser = (req, res) => {
 }
